a041f927583f1da2a76e479da0ede1b8a20bdea4
Emmanuel Tolulope <princetolu11@gmail.com>
2023-06-01 13:08:15 +0100

    second commit

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, profile, RegisterView
+from .views import home, RegisterView
 
 urlpatterns = [
     path('', home, name='users-home'),
     path('register/', RegisterView.as_view(), name='users-register'),
-    path('profile/', profile, name='users-profile'),
 ]
